76afd22f52584012cabc2fe238d79d8f814d836e
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-25 01:58:06 +0500

    server: enable cors for coupons route

--- server/routes/coupons.js
+++ server/routes/coupons.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_TEST);
+const cors = require('./cors');
 
 /* GET coupons list */
-router.get('/', async function (req, res, next) {
-  const coupons = await stripe.coupons.list({
-    limit: 10,
+router
+  .route('/')
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, async function (req, res, next) {
+    const coupons = await stripe.coupons.list({
+      limit: 10,
+    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(coupons);
   });
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json(coupons);
-});
 
 module.exports = router;
