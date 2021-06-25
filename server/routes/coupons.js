9183ecbbea1b308e1daec645f173dd5e99d06c6f
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-25 19:27:07 +0500

    Server(coupon): add coupon api

--- server/routes/coupons.js
+++ server/routes/coupons.js
@@ -18,4 +18,30 @@ router
     res.json(coupons);
   });
 
+/* Create a coupons */
+router
+  .route('/addcoupon')
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .post(cors.corsWithOptions, async function (req, res, next) {
+    /* const couponsParams = {
+      name: req.body.name,
+      percent_off: req.body.percent_off,
+      duration: req.body.duration,
+      duration_in_months: req.body.duration_in_months,
+    }; */
+    try {
+      const coupon = await stripe.coupons.create(req.body);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(coupon);
+    } catch (error) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(error.message);
+      next(error);
+    }
+  });
+
 module.exports = router;
