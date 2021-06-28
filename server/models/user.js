ced25ff0768589af670a160f18d89038b30186b5
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-28 16:18:28 +0500

    Server: Added user model

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
